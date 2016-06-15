7df896a7c53258155a29e4d6d40c39b4ecf2067f
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2016-06-14 22:12:15 -0700

    undo mime type registration, instead include a register for JSONAPI renderer

--- tempo-api/config/initializers/active_model_serializers.rb
+++ tempo-api/config/initializers/active_model_serializers.rb
@@ -1,11 +1,4 @@
+require 'active_model_serializers/register_jsonapi_renderer'
+
 ActiveModelSerializers.config.adapter = :json_api
-ActiveModelSerializers.config.key_transform = :underscore
-
-api_mime_types = %W(
-  application/vnd.api+json
-  text/x-json
-  application/json
-)
-
-Mime::Type.unregister :json
-Mime::Type.register 'application/json', :json, api_mime_types
+ActiveModelSerializers.config.key_transform = :unaltered
